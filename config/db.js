5d4c59925f63aa460f344217b6a6aff379c7c2a1
ArthurJfr <jaffro.arthur@gmail.com>
2025-08-18 22:19:41 +0200

    db

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const db_name = process.env.DEV ? process.env.DB_NAME_DEV : process.env.DB_NAME_
 const pool = mysql.createPool({
   host: db_host,
   user: db_user,
-  //password: db_password,  //Activer sur macos en dev
+  password: db_password,  //Activer sur macos en dev
   database: db_name,
   waitForConnections: true,
   connectionLimit: 10,
