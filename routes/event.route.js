88a377066320c03a7ddf2d8ea14bda1f9193eb3e
ArthurJfr <jaffro.arthur@gmail.com>
2025-08-19 19:11:22 +0200

    get all events by user

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const EventController = require('../controllers/event.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
 
 
 
-router.post('/create', authMiddleware, EventController.createEvent);
+router.post('/create', EventController.createEvent);
 router.get('/:id', EventController.getEventById);
 router.get('/owner/:ownerId', EventController.getEventsByOwnerId);
 router.get('/get-all/:participantId', EventController.getAllEvents);
