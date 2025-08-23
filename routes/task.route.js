7a72e404f5ec84a186ef35f9055e84ef7cbaac9f
ArthurJfr <jaffro.arthur@gmail.com>
2025-08-23 18:38:23 +0200

    require role on task

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const TaskController = require('../controllers/task.controller');
-const { requireAuthAndRole } = require('../middleware/authRole.middleware');
+const authMiddleware = require('../middleware/auth.middleware');
+//const { requireAuthAndRole } = require('../middleware/authRole.middleware');
 
 // Routes pour les tâches
-router.post('/create', requireAuthAndRole('user'), TaskController.createTask);
+router.post('/create', authMiddleware, TaskController.createTask);
 router.get('/:id', TaskController.getTask);
 router.get('/event/:eventId', TaskController.getTasksByEventId);
 router.get('/owner/:ownerId', TaskController.getTasksByOwnerId);
 router.get('/participant/:participantId', TaskController.getTasksByParticipantId);
-router.put('/:id', requireAuthAndRole('user'), TaskController.updateTask);
+router.put('/:id', authMiddleware, TaskController.updateTask);
 
 // Nouvelles routes pour la validation
-router.post('/:id/validate', requireAuthAndRole('user'), TaskController.validateTask);
-router.delete('/:id/validate', requireAuthAndRole('user'), TaskController.unvalidateTask);
+router.post('/:id/validate', authMiddleware, TaskController.validateTask);
+router.delete('/:id/validate', authMiddleware, TaskController.unvalidateTask);
 router.get('/validated-by/:userId', TaskController.getTasksValidatedByUser);
 
 module.exports = router;
